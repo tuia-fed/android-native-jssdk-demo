45a6b6c2ea224ba7448900f7628ee39a668fb48a
wangqiang <wangq@duiba.com.cn>
2019-09-10 14:36:42 +0800

    initial demo

---
+++ app/src/main/java/com/example/jssdk/InsertActivity.java
@@ -0,0 +1,80 @@
+package com.example.jssdk;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.ToastUtils;
+import com.tuia.ad.Ad;
+import com.tuia.ad.DefaultAdCallBack;
+
+
+public class InsertActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private Ad ad = null;
+    private FrameLayout mFlAdWrap;
+    public static String slotId = "307566";
+    public static String appkey = "2AaKHTwmu8zCzVTUJJ6o5TNkHMDb";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+        mFlAdWrap = findViewById(R.id.fl_ad_wrap);
+        TextView tvId = findViewById(R.id.tv_id);
+        tvId.setText("当前页面的广告位id:" + slotId);
+        ad = new Ad(appkey, slotId);
+        ad.init(this, mFlAdWrap, new DefaultAdCallBack() {
+            @Override
+            public void onActivityClose() {
+                ToastUtils.showShort("活动弹窗关闭");
+            }
+
+            @Override
+            public void onRewardClose() {
+                ToastUtils.showShort("奖励弹窗关闭");
+            }
+
+            @Override
+            public void onPrizeClose() {
+                LogUtils.vTag(TAG, "====onPrizeClose=====");
+                ToastUtils.showShort("我的奖品弹窗关闭");
+            }
+
+            @Override
+            public void onPrizeShow() {
+                LogUtils.vTag(TAG, "====onPrizeShow=====");
+                ToastUtils.showShort("我的奖品弹窗显示");
+            }
+        });
+        ad.show();
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        //ad.resetAdSize(newConfig.orientation);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        boolean isConsume = ad.onKeyBack(keyCode, event);
+        if (!isConsume) {
+            return super.onKeyDown(keyCode, event);
+        }
+
+        return isConsume;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ad.destroy();
+    }
+}
---
+++ app/src/main/java/com/example/jssdk/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.jssdk;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebStorage;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.blankj.utilcode.util.SPUtils;
+import com.blankj.utilcode.util.ToastUtils;
+import com.tuia.ad.Ad;
+import com.tuia.ad.DefaultAdCallBack;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    private Ad ad = null;
+
+    public static String slotId = "307524";
+    public static String appkey = "2AaKHTwmu8zCzVTUJJ6o5TNkHMDb";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonSplash = findViewById(R.id.btn_splash);
+        Button buttonJumpInsert = findViewById(R.id.btn_jump_insert);
+        Button buttonClean = findViewById(R.id.btn_clean);
+
+        TextView tvSoltId = findViewById(R.id.tv_soltId);
+        tvSoltId.setText("当前页面的广告位id:" + slotId);
+
+        TextView tvAppkey = findViewById(R.id.tv_appkey);
+        tvAppkey.setText("当前页面的appkey:" + appkey);
+        buttonSplash.setOnClickListener(this);
+        buttonJumpInsert.setOnClickListener(this);
+        buttonClean.setOnClickListener(this);
+
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //线上
+        ad = new Ad(appkey, slotId, "demo");
+
+        ad.init(this, null, new DefaultAdCallBack() {
+            @Override
+            public void onActivityClose() {
+                ToastUtils.showShort("活动弹窗关闭");
+            }
+
+            @Override
+            public void onActivityShow() {
+                super.onActivityShow();
+                ToastUtils.showShort("活动弹窗显示");
+            }
+
+            @Override
+            public void onRewardClose() {
+                ToastUtils.showShort("奖励弹窗关闭");
+            }
+
+            @Override
+            public void onRewardShow() {
+                super.onRewardShow();
+                ToastUtils.showShort("奖励弹窗显示");
+            }
+        });
+        getPermissions();
+
+    }
+
+    private void getPermissions() {
+        /**
+         * 动态获取权限，Android 6.0 新特性，一些保护权限，除了要在AndroidManifest中声明权限，还要使用如下代码动态获取
+         */
+        if (Build.VERSION.SDK_INT >= 23) {
+            int REQUEST_CODE_CONTACT = 101;
+            String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.REQUEST_INSTALL_PACKAGES};
+            //验证是否许可权限
+            for (String str : permissions) {
+                if (this.checkSelfPermission(str) != PackageManager.PERMISSION_GRANTED) {
+                    //申请权限
+                    this.requestPermissions(permissions, REQUEST_CODE_CONTACT);
+                    return;
+                }
+            }
+
+
+        }
+    }
+
+    //  清除 deviceId 缓存
+    public void clearCache(Context context) {
+        SharedPreferences pref = context.getSharedPreferences("tuiad", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.clear();
+        editor.apply();
+        Toast.makeText(context, "缓存清除成功，请重启app", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_splash:
+                //ad.resetSlotId(slotId);
+                ad.show();
+                break;
+            case R.id.btn_jump_insert:
+                startActivity(new Intent(MainActivity.this, InsertActivity.class));
+                break;
+            case R.id.btn_clean:
+                SPUtils.getInstance().clear(true);
+                //清楚webview缓存
+                WebStorage.getInstance().deleteAllData();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        ad.resetAdSize(newConfig.orientation);
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        boolean isConsume = ad.onKeyBack(keyCode, event);
+        if (!isConsume) {
+            return super.onKeyDown(keyCode, event);
+        }
+
+        return isConsume;
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ad.destroy();
+    }
+
+
+}
